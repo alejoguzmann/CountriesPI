b31bc6844530f639f73a66e4618eb9d3147f0d68
alejoguzmann <alejoguzmann20@gmail.com>
2023-11-01 07:46:04 -0300

    config redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter} from 'react-router-dom'
+import store from './redux/store/store.js'
+import { Provider } from 'react-redux'
 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
--- client/src/views/landing/landing.jsx
+++ client/src/views/landing/landing.jsx
@@ -1,13 +1,16 @@
 
 
+
 function Landing() {
 
     return (
       <div>
-        <h1>LANDING</h1>
+        <h1>Countries</h1>
+        <button >Home</button>
       </div>
     )
   }
-  
+
+
   export default Landing
   
